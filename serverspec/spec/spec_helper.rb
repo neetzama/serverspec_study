3d9a0beb874dd2f48a5ff929d682a467cb364660
Hiroya Furusawa <pogbba.6@gmail.com>
2020-07-08 17:00:36 +0900

    user を ubuntu に変更

--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'net/ssh'
 set :backend, :ssh
 
 set :host, ENV['TARGET_IP']
-set :ssh_options, :user => 'ec2-user', :keys => ['~/.ssh/id_rsa']
+set :ssh_options, :user => 'ubuntu', :keys => ['~/.ssh/id_rsa']
 set :require_pty, true
 
 # Disable sudo
